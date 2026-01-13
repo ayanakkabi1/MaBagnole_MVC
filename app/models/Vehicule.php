709e4d6fba9683d7f166d34ed4ac28a12f6432a0
ayanakkabi1 <aynour200609@gmail.com>
2026-01-13 16:48:15 +0100

    feat: initial Vehicule class implementation

--- app/models/Vehicule.php
+++ app/models/Vehicule.php
@@ -2,5 +2,52 @@
 
 class Vehicule 
 {
-    // Vehicule model
+    private int $id;
+    private string $modele;
+    private float $prix;
+    private int $categorie_id;
+    public function __construct(PDO $pdo,int $id = 0,string $modele = '',float $prix = 0,int $categorie_id = 0
+    ) {
+        parent::__construct($pdo);
+        $this->id = $id;
+        $this->modele = $modele;
+        $this->prix = $prix;
+        $this->categorie_id = $categorie_id;
+    }
+     public function __get(string $name)
+    {
+        if (!property_exists($this, $name)) {
+            return null;
+        }
+        return $this->$name;
+    }
+    public function __set(string $name, $value): void
+    {
+        if (!property_exists($this, $name)) {
+            throw new Exception("Propriété $name inexistante");
+        }
+
+        switch ($name) {
+            case 'prix':
+                if (!is_numeric($value) || $value < 0) {
+                    return null;
+                }
+                $this->prix = (float) $value;
+                break;
+
+            case 'modele':
+                if (empty($value)) {
+                    return null;
+                }
+                $this->modele = $value;
+                break;
+
+            case 'categorie_id':
+                $this->categorie_id = (int) $value;
+                break;
+
+            default:
+                $this->$name = $value;
+        }
+}
 }
